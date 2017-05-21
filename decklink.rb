8a05e15df8b64a85093c38645d22071d369a3b34
munfesansev <federicofms@gmail.com>
2017-05-21 13:16:04 +0200

    decklink.rb
    Formula homebrew

---
+++ decklink.rb
@@ -0,0 +1,9 @@
+class Decklink < Formula
+  homepage "https://github.com/munfesansev"
+  url "http://zodiacmultimedia.com/decklinksdk/decklinkfede.tar.gz"
+  sha256 "eb5ac8b71848fd18dd486a632e308e294a7185920bafde25883a13224da902f8"
+
+  def install
+    include.install Dir["include/*"]
+  end
+end
