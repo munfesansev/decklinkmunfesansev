ecca40606362601bd77775a2ce2f6ec0698840ed
munfesansev <federicofms@gmail.com>
2017-05-21 13:19:13 +0200

    Update decklink.rb

--- decklink.rb
+++ decklink.rb
@@ -1,6 +1,6 @@
 class Decklink < Formula
   homepage "https://github.com/munfesansev"
-  url "http://zodiacmultimedia.com/decklinksdk/decklinkfede.tar.gz"
+  url "https://github.com/munfesansev/decklinkmunfesansev/blob/master/decklinkfede.tar.gz"
   sha256 "eb5ac8b71848fd18dd486a632e308e294a7185920bafde25883a13224da902f8"
 
   def install
